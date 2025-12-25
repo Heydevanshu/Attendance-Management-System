40b2f67b74514a23f926524804893375069cde44
Devanshu Uikey <devanshuuikey468@gmail.com>
2025-12-25 14:46:36 +0530

    fix db connection for railway

--- Attendence system/db_connect.py
+++ Attendence system/db_connect.py
@@ -1,15 +1,11 @@
+import os
 import mysql.connector
 
 def get_connection():
-    try:
-        conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="WJ28@krhps",
-            database="teacher_db",
-            port=3306
-        )
-        return conn
-    except Exception as e:
-        print("Database connection failed:", e)
-        return None
+    return mysql.connector.connect(
+        host = os.environ.get("MYSQLHOST"),
+        user = os.environ.get("MYSQLUSER"),
+        password = os.environ.get("MYSQLPASSWORD"),
+        database = os.environ.get("MYSQLDATABASE"),
+        port = int(os.enviorn.get("MYSQLPORT"))
+    )
