12aa52fe898496553adee7680c7b9c6084f4445d
Devanshu Uikey <devanshuuikey468@gmail.com>
2025-12-26 19:06:51 +0530

    Update app.py

--- Attendence_system/app.py
+++ Attendence_system/app.py
@@ -1,7 +1,7 @@
 # app.py
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
-from Attendance_system.db_connect import get_connection
+from Attendence_system.db_connect import get_connection
 from datetime import datetime, timedelta, date
 import secrets
 import math
@@ -748,6 +748,7 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=int(os.environ.get("PORT",5000))
     )
+
 
 
 
