bc814f7e23a64590d1568272ddfa83211c19ab9d
Devanshu Uikey <devanshuuikey468@gmail.com>
2025-12-26 20:29:43 +0530

    Fix environment variable access syntax in db_connect.py
    Updated environment variable access from parentheses to brackets.

--- Attendence_system/db_connect.py
+++ Attendence_system/db_connect.py
@@ -3,14 +3,15 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host = os.environ.get("MYSQLHOST"),
-        user = os.environ.get("MYSQLUSER"),
+        host = os.environ.get["MYSQLHOST"],
+        user = os.environ.get["MYSQLUSER"],
 
-        password = os.environ.get("MYSQLPASSWORD"),
-        database = os.environ.get("MYSQLDATABASE"),
-        port = int(os.environ.get("MYSQLPORT",3306))
+        password = os.environ.get["MYSQLPASSWORD"],
+        database = os.environ.get["MYSQLDATABASE"],
+        port = int(os.environ.get["MYSQLPORT"])
     )
     
+
 
 
 
