dfd924b122fc562bddef57b744ded4ec2b872155
Devanshu Uikey <devanshuuikey468@gmail.com>
2025-12-27 18:13:27 +0530

    Refactor environment variable for railway database
    Updated environment variable access from indexing to using get() method for safer retrieval.

--- Attendence_system/db_connect.py
+++ Attendence_system/db_connect.py
@@ -3,14 +3,15 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host = os.environ["MYSQLHOST"],
-        user = os.environ["MYSQLUSER"],
+        host = os.environ.get("MYSQLHOST"),
+        user = os.environ("MYSQLUSER"),
 
-        password = os.environ["MYSQLPASSWORD"],
-        database = os.environ["MYSQLDATABASE"],
-        port = int(os.environ["MYSQLPORT"])
+        password = os.environ("MYSQLPASSWORD"),
+        database = os.environ("MYSQLDATABASE"),
+        port = int(os.environ("MYSQLPORT"))
     )
     
+
 
 
 
