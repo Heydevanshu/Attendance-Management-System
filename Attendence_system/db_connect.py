c4a57cd0605cd45ae38b308693d1a8ac4a2f1fd4
Devanshu Uikey <devanshuuikey468@gmail.com>
2025-12-27 18:19:18 +0530

    Fix environment variable access in db_connect.py

--- Attendence_system/db_connect.py
+++ Attendence_system/db_connect.py
@@ -4,13 +4,14 @@ import mysql.connector
 def get_connection():
     return mysql.connector.connect(
         host = os.environ.get("MYSQLHOST"),
-        user = os.environ("MYSQLUSER"),
+        user = os.environ.get("MYSQLUSER"),
 
-        password = os.environ("MYSQLPASSWORD"),
-        database = os.environ("MYSQLDATABASE"),
-        port = int(os.environ("MYSQLPORT",3306))
+        password = os.environ.get("MYSQLPASSWORD"),
+        database = os.environ.get("MYSQLDATABASE"),
+        port = int(os.environ.get("MYSQLPORT",3306))
     )
     
+
 
 
 
