217f87a9b35cead64a01a79237aa13d4145abf86
Devanshu Uikey <devanshuuikey468@gmail.com>
2025-12-29 19:51:55 +0530

    Update

--- Attendence_system/app.py
+++ Attendence_system/app.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta, date
 import secrets
 import math
 
+app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "HelloWorld")
 
 # ----------------- Helpers -----------------
@@ -747,6 +748,7 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=int(os.environ.get("PORT",5000))
     )
+
 
 
 
