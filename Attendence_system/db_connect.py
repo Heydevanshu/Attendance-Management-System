1dd95a6de1ceda925e0188aa9db9208bcc898717
Devanshu Uikey <devanshuuikey468@gmail.com>
2025-12-29 19:24:59 +0530

    Enhance database connection handling
    Add default port and handle missing database environment variable.

--- Attendence_system/db_connect.py
+++ Attendence_system/db_connect.py
@@ -2,15 +2,19 @@ import os
 import mysql.connector
 
 def get_connection():
+    port_env = os.environ.get("MYSQLPORT")
+
+    port_to_use = int(port_env) if port_env else 50532
+
     return mysql.connector.connect(
         host = os.environ.get("MYSQLHOST"),
         user = os.environ.get("MYSQLUSER"),
-
         password = os.environ.get("MYSQLPASSWORD"),
-        database = os.environ.get("MYSQL_DATABASE"),
-        port = int(os.environ.get("MYSQLPORT"))
+        database = os.environ.get("MYSQLDATABASE") or os.environ.get("MYSQLDATABASE"),
+        port = port_to_use
     )
     
+
 
 
 
