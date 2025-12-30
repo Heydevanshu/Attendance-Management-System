c9782bc17c16bba59681ff71e58d4c67655082f5
Devanshu Uikey <devanshuuikey468@gmail.com>
2025-12-30 17:37:11 +0530

    Update database connection with auth_plugin
    Added 'auth_plugin' parameter to the database connection.

--- Attendence_system/db_connect.py
+++ Attendence_system/db_connect.py
@@ -4,28 +4,26 @@ from mysql.connector import Error
 
 def get_connection():
     try:
-       
         host = os.environ.get("MYSQLHOST") or "mysql.railway.internal"
         user = os.environ.get("MYSQLUSER") or "root"
-  
         password = os.environ.get("MYSQL_ROOT_PASSWORD") or os.environ.get("MYSQLPASSWORD")
+        
         database = os.environ.get("MYSQL_DATABASE") or "railway"
         
-        if not password:
-            password = "IyzPAnsHRQJWxmaWKHyHAZzlUnjAJEpl"
+        port = 3306 
 
         connection = mysql.connector.connect(
             host=host,
             user=user,
             password=password,
             database=database,
-            port=3306
+            port=port,
+            auth_plugin='mysql_native_password' 
         )
         
         if connection.is_connected():
             return connection
 
     except Error as e:
-        print(f"Connection Error Detail: {e}")
+        print(f"Detailed Connection Error: {e}")
         return None
-
