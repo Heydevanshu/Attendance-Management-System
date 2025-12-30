14eb45f76d07a4cf9ce98cb895deb7d2fa59d1fc
Devanshu Uikey <devanshuuikey468@gmail.com>
2025-12-30 17:41:44 +0530

    Modify database connection settings
    Updated the connection password and changed the authentication plugin.

--- Attendence_system/db_connect.py
+++ Attendence_system/db_connect.py
@@ -6,19 +6,18 @@ def get_connection():
     try:
         host = os.environ.get("MYSQLHOST") or "mysql.railway.internal"
         user = os.environ.get("MYSQLUSER") or "root"
-        password = os.environ.get("MYSQL_ROOT_PASSWORD") or os.environ.get("MYSQLPASSWORD")
+        
+        password = os.environ.get("MYSQL_ROOT_PASSWORD") or "IyzPAnsHRQJWxmaWKHyHAZzlUnjAJEpl"
         
         database = os.environ.get("MYSQL_DATABASE") or "railway"
-        
-        port = 3306 
 
         connection = mysql.connector.connect(
             host=host,
             user=user,
             password=password,
             database=database,
-            port=port,
-            auth_plugin='mysql_native_password' 
+            port=3306,
+            auth_plugin='caching_sha2_password'
         )
         
         if connection.is_connected():
