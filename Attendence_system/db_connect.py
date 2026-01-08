428381f357786d796c143aee60afbda6b9e86ff3
Devanshu Uikey <devanshuuikey468@gmail.com>
2026-01-08 20:47:21 +0530

    Update db_connect.py

--- Attendence_system/db_connect.py
+++ Attendence_system/db_connect.py
@@ -4,22 +4,17 @@ from mysql.connector import Error
 
 def get_connection():
     try:
-        host = "ballast.proxy.rlwy.net"
-        user = "root"
-        password = "IyzPANsHRQJWXmaWKHyHAZzlUnjAJEpl"
-        database = "railway"
-        port = 50532 
-
         connection = mysql.connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=database,
-            port=port,
+            host="ballast.proxy.rlwy.net",
+            user="root",
+            password="IyzPANsHRQJWXmaWKHyHAZzlUnjAJEpl",
+            database="railway",
+            port=50532,
             auth_plugin='mysql_native_password'
         )
         
         if connection.is_connected():
+            print("Successfully connected to Railway DB!")
             return connection
 
     except Error as e:
