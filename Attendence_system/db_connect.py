8b91bc2d7dccece6097d5b76aa575e65b835906b
Devanshu Uikey <devanshuuikey468@gmail.com>
2026-01-09 18:18:31 +0530

    Update db_connect.py

--- Attendence_system/db_connect.py
+++ Attendence_system/db_connect.py
@@ -1,28 +1,18 @@
-import os
 import mysql.connector
 from mysql.connector import Error
 
 def get_connection():
-    try:
-        host = os.environ.get("MYSQLHOST") or "mysql.railway.internal"
-        user = os.environ.get("MYSQLUSER") or "root"
-        
-        password = os.environ.get("MYSQL_ROOT_PASSWORD") or "IyzPAnsHRQJWxmaWKHyHAZzlUnjAJEpl"
-        
-        database = os.environ.get("MYSQL_DATABASE") or "railway"
-
-        connection = mysql.connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=database,
-            port=50532,
-            auth_plugin='caching_sha2_password'
-        )
-        
-        if connection.is_connected():
-            return connection
-
-    except Error as e:
-        print(f"Detailed Connection Error: {e}")
-        return None
+    try:
+        connection = mysql.connector.connect(
+            host="ballast.proxy.rlwy.net",
+            user="root",
+            password="IyzPAnsHRQJWxmaWKHyHAZzlUnjAJEpl",
+            database="railway",
+            port=50532,
+            auth_plugin='mysql_native_password'
+        )
+        if connection.is_connected():
+            return connection
+    except Error as e:
+        print(f"Detailed Connection Error: {e}")
+        return None
